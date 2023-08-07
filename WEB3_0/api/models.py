285a01bd1010c196544e820167989592c12079f0
Yedige Mussa <Yadige.mussa@erg.kz>
2023-08-07 12:17:05 +0600

    I changed the User_IIN model to CharField, because did not accept the first digit zero

--- WEB3_0/api/models.py
+++ WEB3_0/api/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class User_Model(models.Model):
     User_Name = models.CharField(max_length=15, )
-    User_IIN = models.IntegerField(max_length=12, unique=True)
+    User_IIN = models.CharField(max_length=12, unique=True)
     All_money = models.FloatField(default=0)
 
     def __str__(self) -> object:
